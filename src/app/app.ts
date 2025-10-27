54d7946b988df7ba9031dc1b61f86b3c850c1c4a
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-10-27 19:50:00 +0100

    Actualización

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import {Login} from './login/login';
 import {Register} from './register/register';
 import {Tareas} from "./tareas/tareas";
+import {Practicando} from "./practicando/practicando";
 
 @Component({
   selector: 'app-root',
